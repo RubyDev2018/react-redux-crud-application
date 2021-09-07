1426f6c1582d607d5056b328041963db3e37d90e
kenjikojima0921 <k.kojima@omotesando-skin.jp>
2021-09-07 23:31:03 +0900

    create class coomponent function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 // JSX:直感的にHTMLを表現ができるため、JSではなく、JSX(xlm)を使用
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am Chinko")}} />
-      </React.Fragment>
-    )
-  }
-}
-
-// JS
 // class App extends Component {
 //   render() {
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello, world!!!"
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am Chinko")}} />
+//       </React.Fragment>
 //     )
 //   }
 // }
+
+// function components
+const App = () => { 
+  return ( 
+    <>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </>
+  )
+}
+
+const Cat = () => { 
+  return <div>Meowo</div>
+}
 
 export default App;
