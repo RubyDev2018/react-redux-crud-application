9f41c1fd5e5c4a939d1cf12ed3ad74bf1891ba79
kenjikojima0921 <k.kojima@omotesando-skin.jp>
2021-09-07 23:15:41 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React, {Component} from 'react';
 // JSX:直感的にHTMLを表現ができるため、JSではなく、JSX(xlm)を使用
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am Chinko")}} />
+      </React.Fragment>
+    )
   }
 }
 
